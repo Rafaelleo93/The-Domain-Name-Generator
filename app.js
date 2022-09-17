7e704fba20fcbe246f5521a8c71ebecfcb873ca9
Rafaelleo93 <rafaelleonardo93@gmail.com>
2022-09-17 17:05:42 +0000

    Domain Name Gen

---
+++ app.js
@@ -0,0 +1,14 @@
+let pronoun = ["the", "our"];
+  let adj = ["great", "big", "more", "best"];
+  let noun = ["jogger", "racoon", "sport"];
+  let dot = [".com", ".es", ".io", ".net"]
+
+  for (let i in pronoun) {
+    for (let a in adj) {
+      for (let b in noun) {
+        for (let c in dot){
+            console.log(pronoun[i] + adj[a] + noun[b] + dot[c]);
+        }
+      }
+    }
+  }
